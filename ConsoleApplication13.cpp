e526ddd40bfecc19fb743d0707bf0703bab86297
yan847 <61082486+yan847@users.noreply.github.com>
2021-09-30 12:26:26 +0300

    Add files via upload

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,126 @@
+﻿// ConsoleApplication13.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+
+using namespace std;
+
+class Figure {
+
+public:
+
+
+
+	virtual void calc_square() const = 0;
+	virtual void calc_perimeter() const = 0;
+};
+
+class Triangle : public Figure { //(треугольник)
+
+private:
+
+	double a, b, c;
+
+public:
+
+
+
+	void calc_square() const { //площади треугольника           
+		double p = a + b + c;
+		cout << sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+
+	void calc_perimeter() const { // периметра треугольника            
+		cout << a + b + c;
+	}
+
+
+
+	Triangle(double side_a, double side_b, double side_c)
+		: a(side_a), b(side_b), c(side_c) {}
+};
+
+class Circle : public Figure { //(круг)
+
+private:
+
+	double r;
+
+public:
+
+
+
+	void calc_square() const { // площади (круга)           
+		cout << 3, 14 * r * r;
+	}
+
+	void calc_perimeter() const { // периметр (круга)            
+		cout << 2 * 3, 14 * r;
+	}
+
+
+
+	Circle(double radius)
+		: r(radius) {}
+};
+
+
+class Rectangle : public Figure { // (прямоугольник)
+
+private:
+
+	double a, b;
+
+public:
+
+
+
+	void calc_square() const { // площад (прямоугольника)           
+		cout << a * b;
+	}
+
+	void calc_perimeter() const { // периметр (прямоугольника)            
+		cout << 2 * (a + b);
+	}
+
+
+
+	Rectangle(double side_a, double side_b)
+		: a(side_a), b(side_b) {}
+};
+
+
+
+int main() {
+	Figure** figure = new Figure*[5];
+	cout << "Output of the program:\n";
+	cout << "\nFigure #1 (triangle):";
+	figure[0] = new Triangle(3, 4, 5);
+	cout << "\nSquare: ";
+	figure[0]->calc_square();
+	cout << "\nPerimeter: ";
+	figure[0]->calc_perimeter();
+
+	cout << "\n\nFigure #2 (circle):";
+	figure[1] = new Circle(5);
+	cout << "\nSquare: ";
+	figure[1]->calc_square();
+	cout << "\nPerimeter: ";
+	figure[1]->calc_perimeter();
+
+
+
+	cout << "\n\nFigure #3 (rectangle):";
+	figure[2] = new Rectangle(4, 5);
+	cout << "\nSquare: ";
+	figure[2]->calc_square();
+	cout << "\nPerimeter: ";
+	figure[2]->calc_perimeter();
+	delete figure[0];
+	delete figure[1];
+	delete figure[2];
+	delete[] figure;
+	return 0;
+}
---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,86 @@
+﻿// ConsoleApplication8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Payment
+{
+private:
+	char fio[100]; //Фио
+	double oklad; //оклад
+	int year; //год поступления на работу
+	double procent; //процент надбавки
+	double nalog; //подоходный налог
+	int rab_day_month; //кол-во отработанных дней
+	int day_month; //кол-во рабочих дней
+	double nach_summ; //начисленная сумма
+	double yder_summ; //удержанная сумма
+	int stag; //стаж (полное кол-во лет)
+	double zp; //зарплата
+public:
+	double vichisl_nach_summ();
+	double vichisl_yder_summ();
+	double vichisl_zp();
+	int vichisl_stag();
+	void set();
+	void show();
+};
+
+double Payment::vichisl_nach_summ()
+{
+	return (oklad*rab_day_month);
+}
+
+double Payment::vichisl_yder_summ()
+{
+	return (nach_summ*0.01 + nach_summ * 0.13);
+}
+
+double Payment::vichisl_zp()
+{
+	return (rab_day_month*oklad - (rab_day_month*oklad*0.13));
+}
+
+int Payment::vichisl_stag()
+{
+	return (2009 - year);
+
+}
+
+void Payment::set()
+{
+	cout << "Введите фамилию сотрудника - ";
+	cin >> fio;
+	cout << "Введите оклад сотрудника  - ";
+	cin >> oklad;
+	cout << "Введите год поступления сотрудника на работу - ";
+	cin >> year;
+	cout << "Введите процент надбавки - ";
+	cin >> procent;
+	cout << "Введите кол-во отработанных дней - ";
+	cin >> rab_day_month;
+
+}
+
+
+
+void Payment::show()
+{
+	cout << "Начисленная сумма составляет: " << vichisl_nach_summ() << "\n";
+	cout << "Удержанная сумма составляет: " << vichisl_yder_summ() << "\n";
+	cout << "Сумма, выдаваемая на руки составляет: " << vichisl_zp() << "\n";
+	cout << "Стаж составляет: " << vichisl_stag() << "\n";
+}
+
+
+int main()
+{
+	setlocale(LC_CTYPE, ".1251");
+	Payment n1;
+	n1.set();
+	n1.show();
+}
